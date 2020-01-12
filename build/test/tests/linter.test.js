9769258bb83efe8e705463b6b09559ff8c3443e9
letulip <ivladimirskiy@ya.ru>
2020-01-12 15:50:57 +0300

    feat: add object destruct functional

--- build/test/tests/linter.test.js
+++ build/test/tests/linter.test.js
@@ -2,9 +2,42 @@
 
 var chai = require('chai');
 
-const warningTest = (warningBlock) => {
+const objectDestruct = (object) => {
+  if (object.block) {
+    console.log(`block name: ${object.block}`);
+  }
+
+  if (object.content) {
+    object.content.forEach((contentElement) => {
+      
+      // if (contentElement) {
+      //   console.log(contentElement);
+      // }
+
+      if (contentElement.elem) {
+        console.log(`element name: ${contentElement.elem}`);
+      }
+
+      if (typeof contentElement === `object`) {
+        objectDestruct(contentElement);
+      }
+    });
+  } 
+
+  if (object.mods) {
+    // objectDestruct(object.mods);
+    console.log(object.mods);
+  }
   
-  console.log(JSON.parse(warningBlock).content);
+};
+
+const warningTest = (warningBlock) => {
+  const object = JSON.parse(warningBlock);
+
+  objectDestruct(object);
+  // console.log(object);
+  // console.log(object.block);
+  // console.log(object.content);
   
   return [];
 };
--- src/js/linter.js
+++ src/js/linter.js
@@ -5,10 +5,42 @@ const testSizeEqual = (textBlockArray) => {
   });
 };
 
-const warningTest = (warningBlock) => {
-  const block = JSON.parse(warningBlock);
+const objectDestruct = (object) => {
+  if (object.block) {
+    console.log(`block name: ${object.block}`);
+  }
 
-  console.log(block);
+  if (object.content) {
+    object.content.forEach((contentElement) => {
+      
+      // if (contentElement) {
+      //   console.log(contentElement);
+      // }
+
+      if (contentElement.elem) {
+        console.log(`element name: ${contentElement.elem}`);
+      }
+
+      if (typeof contentElement === `object`) {
+        objectDestruct(contentElement);
+      }
+    });
+  } 
+
+  if (object.mods) {
+    // objectDestruct(object.mods);
+    console.log(object.mods);
+  }
+  
+}
+
+const warningTest = (warningBlock) => {
+  const object = JSON.parse(warningBlock);
+
+  objectDestruct(object);
+  // console.log(object);
+  // console.log(object.block);
+  // console.log(object.content);
   
   return [];
 };
